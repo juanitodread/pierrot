09cfeae18662021ae9b41c113d8a46b9b498536d
Juan Sandoval <1304670+juanitodread@users.noreply.github.com>
2024-03-14 09:48:54 -0700

    Implement logic for sync-db lambda (#14)

--- lambdas/sync_db.py
+++ lambdas/sync_db.py
@@ -1,10 +1,25 @@
+from pierrot.src.clients.flickr.client import Flickr
+from pierrot.src.clients.s3.client import S3
+from pierrot.src.config import Config
 from pierrot.src.logging import get_logger
+from pierrot.src.pierrot import Pierrot
 
 
 log = get_logger(__name__)
 
 
-def do_work(event, context):
-  log.info(f'Event: {event}')
+def do_work(event, context) -> None:
+  log.info(f'Event: {event}, Context: {context}')
 
-  log.info('sync db function deployed')
+  flickr_config = Config.get_flickr_config()
+  s3_config = Config.get_s3_config()
+
+  flickr = Flickr(flickr_config)
+  s3 = S3(s3_config)
+
+  pierrot = Pierrot(
+    flickr_username=flickr_config.photos_owner,
+    flickr=flickr,
+    s3=s3,
+  )
+  pierrot.sync_db()
