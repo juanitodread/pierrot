b764a31a764739ea42cd7715bfa806abdcad4a28
juanitodread <juanitodread@gmail.com>
2024-03-14 17:17:48 -0700

    Add lambda for publishing a photo

---
+++ lambdas/publish_photo.py
@@ -0,0 +1,29 @@
+from pierrot.src.clients.flickr.client import Flickr
+from pierrot.src.clients.s3.client import S3
+from pierrot.src.clients.twitter.client import Twitter
+from pierrot.src.config import Config
+from pierrot.src.logging import get_logger
+from pierrot.src.pierrot import Pierrot
+
+
+log = get_logger(__name__)
+
+
+def do_work(event, context) -> None:
+  log.info(f'Event: {event}, Context: {context}')
+
+  flickr_config = Config.get_flickr_config()
+  s3_config = Config.get_s3_config()
+  twitter_config = Config.get_twitter_config()
+
+  flickr = Flickr(flickr_config)
+  s3 = S3(s3_config)
+  twitter = Twitter(twitter_config)
+
+  pierrot = Pierrot(
+    flickr_username=flickr_config.photos_owner,
+    flickr=flickr,
+    s3=s3,
+    twitter=twitter,
+  )
+  pierrot.publish_photo()
